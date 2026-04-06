e0b1321a03da95717481583b5d81bc140b264e20
Rin <playmurka86@gmail.com>
2026-04-06 11:35:21 +0500

    a

--- Assets/Scripts/Systems/TR1AI.cs
+++ Assets/Scripts/Systems/TR1AI.cs
@@ -10,6 +10,7 @@ public class TR1Component : IComponent
     public SpriteRenderer line,attackSprite,BaseSprite;
     public float attackDistance = 5f;
     public float attackSpeed = 10f;
+    public EventSound attackSound;
 
     public Sprite[] RotAnimation;
 } 
@@ -19,6 +20,8 @@ public class TR1AI : BaseAI
 {
     private TargetSearchComponent trc;
     private TR1Component tr1;
+    private AnimationComponent _animationComponent;
+    private HealthComponent HealthComponent;
 
     private Action<InputContext> attackAction;
     private std.Optional<Coroutine> attackRoutine;
@@ -30,12 +33,13 @@ public class TR1AI : BaseAI
 
         trc = owner.GetControllerComponent<TargetSearchComponent>();
         tr1 = owner.GetControllerComponent<TR1Component>();
+        HealthComponent = owner.GetControllerComponent<HealthComponent>();
+        _animationComponent = owner.GetControllerComponent<AnimationComponent>();
 
         trc.onTargetChange += OnFindTarget;
 
         attackAction += context =>
         {
-            Debug.Log(attackRoutine.Enabled);
             if (!attackRoutine.Enabled)
             {
                 attackRoutine = Optional<Coroutine>.Some(owner.StartCoroutine(AttackRoutine()));
@@ -43,6 +47,11 @@ public class TR1AI : BaseAI
         };
 
         GetState().Attack.performed += attackAction;
+        HealthComponent.OnDie += entity =>
+        {
+            if (attackRoutine.Enabled)
+                owner.StopCoroutine(attackRoutine.Value);
+        };
     }
 
     public void OnFindTarget(Transform target)
@@ -53,18 +62,25 @@ public class TR1AI : BaseAI
 
     private IEnumerator AttackRoutine()
     {
+        _animationComponent.Play("Find");
+        
+        yield return new WaitUntil(() => _animationComponent.GetProgress(0) < 0.2f);
+        yield return new WaitUntil(() => _animationComponent.GetProgress(0) >= 1);
+        _animationComponent.Play("Attack");
         Vector3 startSize = tr1.line.size;
         Vector3 startPos = tr1.attackSprite.transform.localPosition;
         float t = 0f;
         int i = 0;
         int len = tr1.RotAnimation.Length;
-
+        
+        _animationComponent.SetAnimationSpeed(1);
+        AudioManager.instance.PlayEvent(new EventSoundInstance(tr1.attackSound));
         // Расширение линии вверх
         while (t < 1f)
         {
             t += Time.deltaTime * tr1.attackSpeed;
             float eased = Mathf.SmoothStep(0f, 1f, Mathf.Clamp01(t));
-            tr1.line.size = new Vector3(startSize.x, Mathf.Lerp(0f, tr1.attackDistance, eased), 1f);
+            tr1.line.size = new Vector3(startSize.x, Mathf.Lerp(startSize.y, tr1.attackDistance, eased), 1f);
             tr1.attackSprite.transform.localPosition = startPos + Vector3.up * (tr1.line.size.y - startPos.y);
 
             // Анимация спрайта по кругу
@@ -75,25 +91,25 @@ public class TR1AI : BaseAI
         }
 
         t = 0f;
-
         // Сжатие линии обратно
         while (t < 1f)
         {
             t += Time.deltaTime * tr1.attackSpeed;
             float eased = Mathf.SmoothStep(0f, 1f, Mathf.Clamp01(t));
-            tr1.line.size = new Vector3(startSize.x, Mathf.Lerp(tr1.attackDistance, 0f, eased), 1f);
+            tr1.line.size = new Vector3(startSize.x, Mathf.Lerp(tr1.attackDistance, startSize.y, eased), 1f);
             tr1.attackSprite.transform.localPosition = startPos + Vector3.up * (tr1.line.size.y - startPos.y);
-
-            // Анимация спрайта по кругу назад
+            
             tr1.BaseSprite.sprite = tr1.RotAnimation[i];
-            i = (i - 1 + len) % len; // безопасно, не выйдет за границу
+            i = (i - 1 + len) % len;
 
             yield return null;
         }
 
         // Сброс позиции и размера
+        _animationComponent.SetAnimationSpeed(1);
         tr1.line.size = startSize;
         tr1.attackSprite.transform.localPosition = startPos;
+        _animationComponent.Play("Idle");
         attackRoutine = Optional<Coroutine>.None();
     }
 }
